3d93d5abbe239efa20d837aeed97fa28d8c353ba
Lean Obregon <lean.obregon1@gmail.com>
2023-06-25 17:20:26 -0400

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (i = 0; i < array.length; i++) {
+        return array.filter(x => !args.includes(x))
+    }
 };
 
 // Do not edit below this line
