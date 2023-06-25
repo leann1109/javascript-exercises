c59b11f6e5fcec3514d9d9d64e3ba5c520f5e693
Lean Obregon <lean.obregon1@gmail.com>
2023-06-25 15:56:24 -0400

    completed excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let stringResult = "";
 
-};
-
+    if (num > 0) {
+        for (i = 0; i < num; i++) {
+            stringResult += string;
+        }
+        return stringResult;
+    }
+    else if (num == 0) {
+        return stringResult;
+    }
+    else {
+        return "ERROR";
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
