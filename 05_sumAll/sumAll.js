1895dd2e2f10f3be1996843d9ab91ea5b9e7e9e4
Lean Obregon <lean.obregon1@gmail.com>
2023-06-25 17:38:31 -0400

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let sumAllEqualTo = 0;
+    let testForNegative = num1 + num2;
 
+    if (num1 < num2 && testForNegative > 0 
+        && typeof num1 === "number" && typeof num2 === "number") {
+        for (i = num1; i <= num2; i++) {
+            sumAllEqualTo += i;
+        }
+        return sumAllEqualTo;
+    }
+    else if (num1 > num2 && testForNegative > 0 
+        && typeof num1 === "number" && typeof num2 === "number") {
+        for (i = num1; i >= num2; i--) {
+            sumAllEqualTo += i;
+        }
+        return sumAllEqualTo;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
