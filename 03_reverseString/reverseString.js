a4882b0d2386ce5fe58a03acd5e5065afad860f3
Lean Obregon <lean.obregon1@gmail.com>
2023-06-25 16:06:01 -0400

    completed exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringInReverse = "";
 
+    if (string.length > 0) {
+        for (i = string.length; i >= 0; i--) {
+            stringInReverse += string.charAt(i);
+        }
+        return stringInReverse;
+    }
+    else {
+        return stringInReverse;
+    }
 };
 
 // Do not edit below this line
