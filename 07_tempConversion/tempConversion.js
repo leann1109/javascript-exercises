8b496ab4d9977b91f728ee646139b1c6c01913b9
Lean Obregon <lean.obregon1@gmail.com>
2023-06-26 00:53:23 -0400

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = (temp - 32) * (5/9);
+  let resultRounded = Math.round(result * 10) / 10;
+  return resultRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (temp * (9/5) + 32);
+  let resultRounded = Math.round(result * 10) / 10;
+  return resultRounded;
 };
 
 // Do not edit below this line
