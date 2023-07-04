661333713096e3697ad6571e37cf097803219e06
Lean Obregon <lean.obregon1@gmail.com>
2023-07-04 00:38:01 -0400

    completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let result = num1 + num2;
+	return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let result = num1 - num2;
+	return result;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+	let result = 0;
+  if (num.length === 0) {
+    return result;
+  }
+  for (i = 0; i < num.length; i++) {
+    result += num[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let result = 1;
+  for (i = 0; i < args.length; i++) {
+    result *= args[i];
+  }
+	return result;
 };
 
-const power = function() {
-	
+const power = function(num, numTimes) {
+	let result;
+  let currentResult;
+  for (i = 1; i < numTimes; i ++) {
+    currentResult = num * num;
+    result = currentResult * num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  if (n === 0) {
+    return result = 1;
+  }
+  
+  for (i = n; i > 0; i--) {
+    result *= i;
+    }
+  return result;
 };
 
 // Do not edit below this line
